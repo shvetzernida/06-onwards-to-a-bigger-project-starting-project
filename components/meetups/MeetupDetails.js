2827c2f43fd8af230af2baaf796ea6396246daae
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2024-02-14 04:42:13 -0600

    add: Image in MeetupDetails

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,11 +1,14 @@
-import classes from './MeetupDetails.module.css';
+import Image from "next/image";
+import classes from "./MeetupDetails.module.css";
 
 function MeetupDetails(props) {
   const { title, image, address, description } = props ?? {};
   return (
-    <div className={classes['details']}>
+    <div className={classes["details"]}>
       <h1>{title}</h1>
-      <img src={image} alt={title} className={classes['image']} />
+      <div className={classes["image-container"]}>
+        <Image src={image} alt={title} className={classes["image"]} fill />
+      </div>
       <address>{address}</address>
       <p>{description}</p>
     </div>
